4ef2113bd4ecf845621f6cbaf47a4e29ce7a54c0
szymanskir <ryszard.szymanski@outlook.com>
2019-04-02 21:36:54 +0200

    Init project:
    * create basic project structure
    * add basic requirements
    * create makefile for creating venv

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='convcifar',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for Deep Learning classes at the Warsaw University of Technology',
+    author='Paweł Rzepiński, Ryszard Szymański',
+    license='MIT',
+)
